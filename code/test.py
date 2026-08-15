5aad3073b0a813a1056ba2c3935f8dc934858ee4
Zjr070523-code <Zjr070523@163.com>
2026-08-15 21:36:04 +0800

    第二天语句语法练习

--- code/test.py
+++ code/test.py
@@ -4,8 +4,18 @@ evens = [x for x in range(1,21) if x % 2 == 0]
 print("1到20的偶数:",evens)
 student = {"name" :"张三", "age":"20","course":["数学","英语","信息"]}
 print("学生姓名:",student.get("name"))
-print("学生年龄:",student.get("name"))
+print("学生年龄:",student.get("age"))
 print("学生选课:",student.get("course"))
 score = student.get("score",0)
 print("成绩:",score)
 print(f"学生{student['name']}今年{student['age']}岁,选择了{len(student['course'])}门课")
+score = 86
+if score >= 90:
+ grade = "A"
+elif score >=80:
+ grade = "B"
+else :
+ grade = "C"
+print(f"成绩:{score}对应等级是{grade}")
+for key,value in student .items():
+    print(f"{key}:{value}")
