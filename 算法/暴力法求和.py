0ee4549846cb4f794ee79d8ee78826e6ff174b0a
Zjr070523-code <Zjr070523@163.com>
2026-08-31 18:24:15 +0800

    算法第一天：暴力法求和

---
+++ 算法/暴力法求和.py
@@ -0,0 +1,10 @@
+def two_sum(nums,target):
+    n = len(nums)
+    for i in range(n):
+        for j in range(i+1,n):
+            if nums[i]+nums[j] == target:
+                return [i,j]
+    return []
+print(two_sum([1,2,3,4,5],6))
+print(two_sum([1,2,3,4,5],10))
+
